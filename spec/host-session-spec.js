f8e321a704872731228422dba080e23f319b7973
Philipp Andronov <filipp.andronov@gmail.com>
2014-03-11 15:02:57 +0700

    Host session statistics

--- spec/host-session-spec.js
+++ spec/host-session-spec.js
@@ -4,9 +4,11 @@ describe('Host crawling session', function(){
 	it('has defaults', function(){
 		var session = new HostSession();
 
-		expect(session._crawlDelay).toBe(1000);
+		expect(session.totalRequestsCount).toBe(0);
+		expect(session.awaitingRequestsCount).toBe(0);
+		expect(session.activeRequestsCount).toBe(0);
+		expect(session.crawlDelay).toBe(1000);
 		expect(session._rules).toEqual({});
-		expect(session._sentRequestCount).toBe(0);
 
 		expect(session._lastRequestTime).toBe(false);
 		expect(session.timeToWait()).toBe(0);
@@ -20,27 +22,31 @@ describe('Host crawling session', function(){
 		expect(function(){session.selectIp()}).toThrow();
 		expect(function(){session.selectIp([])}).toThrow();
 
+		for (var i=0; i < 20; i++) {
+			session.requestAdded();
+		}
+
 		// Test that 10 requests for a single IP is always
 		// single IP
 		for (var i=0; i < 10; i++) {
 			expect(session.selectIp(['127.0.0.1'])).toBe('127.0.0.1');
-			session.requestSent();
+			session.requestBegin();
 		}
-		expect(session._sentRequestCount).toBe(10);
+		expect(session.activeRequestsCount).toBe(10);
 
 		// Test that for multiple IP first 9 goes to first
 		// and 10th is always to second
 		var list=['127.0.0.1','127.0.0.2'];
 		for (var i=0; i < 9; i++) {
 			expect(session.selectIp(list)).toBe('127.0.0.1');
-			session.requestSent();
+			session.requestBegin();
 		}
-		expect(session._sentRequestCount).toBe(19);
+		expect(session.activeRequestsCount).toBe(19);
 		expect(session.selectIp(list)).toBe('127.0.0.2');
 
-		session.requestSent();
+		session.requestBegin();
 
-		expect(session._sentRequestCount).toBe(20);
+		expect(session.activeRequestsCount).toBe(20);
 		expect(session.selectIp(list)).toBe('127.0.0.1');
 	});
 
