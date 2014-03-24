767d9ab7d66d6372637fd75be77aa9c27663c8d2
Philipp Andronov <filipp.andronov@gmail.com>
2014-03-24 23:36:46 +0700

    #9 some tests for the crawl sequence

--- spec/crawler-offline-spec.js
+++ spec/crawler-offline-spec.js
@@ -16,4 +16,75 @@ describe('Crawler instance', function() {
 		this.addMatchers(matchers);
 	});
 
+	// Fetch result to be used in tests
+	var result = {
+		status: 200,
+		statusText: 'OK',
+
+		request: {
+
+		},
+
+		response: {
+			'server': 'nginx',
+			'transfer-encoding': 'chunked',
+			'connection': 'close',
+			'vary': 'Accept-Encoding',
+			'date': 'Mon, 24 Mar 2014 15:01:52 GMT',
+			'content-type': 'text/html; charset=UTF-8',
+			'set-cookie': ['geo_location=a%3A3%3A%7Bs%3A7%3A%22city_id%22%3Ba%3A0%3A%7B%7Ds%3A9%3A%22region_id%22%3Ba%3A0%3A%7B%7Ds%3A10%3A%22country_id%22%3Ba%3A0%3A%7B%7D%7D; Domain=.auto.ru; expires=Tue, 24-Mar-2015 15:01:52 GMT; Version=1; Path=/; HttpOnly=1'],
+			'content-encoding': 'gzip'
+		},
+
+		timings: {
+			connect: 10,
+			sent: 5,
+			receive: 15
+		},
+
+		content: {
+			size: 27,
+			unpacked: 27,
+			data: 'Hello-World!\nTHat is a test'
+		},
+
+		processed: {
+			outLinks: [],
+			language: 'unknown',
+			confidence: 0.8
+		}
+	};
+
+	it('aggregates fetch results into the trace', function(done){
+		var crawler = new Crawler();
+		crawler.setup('fetchPageContent', function(trace, callback){
+			expect(this).toBe(crawler);
+			callback(null, result);
+		});
+
+		crawler.crawl(['http://google.com']).then(function(result){
+			expect(result.pages.length).toBe(1);
+			expect(result.entries.length).toBe(1);
+
+			var entry = result.entries[0];
+
+			expect(entry).toBeDefined();
+			expect(entry.response.status).toBe(200);
+			expect(entry.response.statusText).toBe('OK');
+			expect(entry.response.redirectURL).toEqual('');
+			expect(entry.response.cookies).toEqual([]);
+			expect(entry.response.headersSize).toBe(201);
+			expect(entry.response.headers).toEqual([
+                    { name : 'server', value : 'nginx' },
+					{ name : 'transfer-encoding', value : 'chunked' },
+					{ name : 'connection', value : 'close' },
+					{ name : 'vary', value : 'Accept-Encoding' },
+					{ name : 'date', value : 'Mon, 24 Mar 2014 15:01:52 GMT' },
+					{ name : 'content-type', value : 'text/html; charset=UTF-8' },
+					{ name : 'set-cookie', value : 'geo_location=a%3A3%3A%7Bs%3A7%3A%22city_id%22%3Ba%3A0%3A%7B%7Ds%3A9%3A%22region_id%22%3Ba%3A0%3A%7B%7Ds%3A10%3A%22country_id%22%3Ba%3A0%3A%7B%7D%7D; Domain=.auto.ru; expires=Tue, 24-Mar-2015 15:01:52 GMT; Version=1; Path=/; HttpOnly=1' },
+					{ name : 'content-encoding', value : 'gzip' }
+			]);
+			done();
+		}, done).otherwise(done);
+	});
 });
